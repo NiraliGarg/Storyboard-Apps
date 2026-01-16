fdcedc2cbb12256134c561d2c0c18412a00a5029
NiraliGarg <162393863+NiraliGarg@users.noreply.github.com>
2026-01-16 17:33:19 +0530

    Add files via upload

---
+++ TrafficS/TrafficS/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TrafficS
+//
+//  Created by GEU on 06/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    segue.destination.navigationItem.title = TextField.text
+        }
+    
+
+}
+
