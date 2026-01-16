2064fefd709833936e9214f225a1dfac894c7e22
NiraliGarg <162393863+NiraliGarg@users.noreply.github.com>
2026-01-16 17:33:13 +0530

    Add files via upload

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by GEU on 05/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
