381f93e3dd739c58db3beb71df4856094abf8e9c
NiraliGarg <162393863+NiraliGarg@users.noreply.github.com>
2026-05-27 14:06:28 +0530

    Add files via upload

---
+++ MealTracker/MealTracker/Food.swift
@@ -0,0 +1,12 @@
+//
+//  Food.swift
+//  MealTracker
+//
+//  Created by GEU on 07/01/26.
+//
+
+import Foundation
+struct Food {
+    let name: String
+    let description: String
+}
---
+++ MealTracker/MealTracker/Meal.swift
@@ -0,0 +1,12 @@
+//
+//  Meal.swift
+//  MealTracker
+//
+//  Created by GEU on 07/01/26.
+//
+
+import Foundation
+struct Meal {
+    let name: String
+    let food: [Food]
+}
